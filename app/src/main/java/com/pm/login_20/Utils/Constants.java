bbc1de910d4948bfb35aea1a466519ce0784b0dd
NandoCodeDS <99371351+NandoCodeDS@users.noreply.github.com>
2022-10-14 23:36:04 -0500

    Ajuste BASE_URL

--- app/src/main/java/com/pm/login_20/Utils/Constants.java
+++ app/src/main/java/com/pm/login_20/Utils/Constants.java
@@ -1,15 +1,6 @@
 package com.pm.login_20.Utils;
 
 public class Constants {
-    //public static final String BASE_URL = "http://localhost:8080/";
-
-    //public static final String BASE_URL = "http://192.168.56.1:5432/";
-    //public static final String BASE_URL = "http://192.168.56.1:8080/postgres/";
-    //public static final String BASE_URL = "http://10.0.2.2:8080/postgres/";
-    //public static final String BASE_URL = "http://10.0.2.2:8080/";
-    //public static final String BASE_URL = "http://192.168.1.104:8080/";
     public static final String BASE_URL = "http://192.168.1.104:8080";
-    //172.27.32.1
-
-
+    //Esta es la Ip de Ethernet (cmd/ipconfig)
 }
